839360b0f16607d1f3e8427d61ad4fa8179069ba
Gabrielegava <gabrielegava123@gmail.com>
2022-09-06 11:36:02 -0300

    executar chatclientepetshopControllers

--- controllers/chatclientepetshopController.js
+++ controllers/chatclientepetshopController.js
@@ -5,7 +5,9 @@ const db = require("../database/conection");
 module.exports = {
     async listarChatclientepetshop(request, response) {
         try{
-            return response.status(200).json({confirma: 'Chat Cliente PetShop'});
+           const sql = 'select chat_id, cliente_id, chat_data, chat_msg from chat_cliente_petshop;';
+           const chat_cliente_petshop = await db.query(sql);
+            return response.status(200).json({confirma: 'Sucesso', nResults: chat_cliente_petshop[0].length, message: chat_cliente_petshop[0]});
         } catch (error) {
             return response.status(500).json({confirma : 'Erro', message: error});
         }
