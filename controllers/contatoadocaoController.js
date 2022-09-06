cd2e1d08c23839f4fdcdb1b8b125e678dd00f509
Guzinxd <110039773+Guzinxd@users.noreply.github.com>
2022-09-06 11:35:44 -0300

    listar contato adocao

--- controllers/contatoadocaoController.js
+++ controllers/contatoadocaoController.js
@@ -5,8 +5,10 @@ const db = require("../database/conection");
 
 module.exports = {
     async listarContatoadocao(request, response) {
-        try{
-            return response.status(200).json({confirma: 'Contato Adocao'});
+        try{const sql = 'SELECT ctt_id, adc_id, ctt_data, ctt_msg from contato_adocao;';
+        const contato_adocao = await db.query(sql);
+
+        return response.status(200).json({confirma: 'Sucesso', nResults: contato_adocao[0].length, message: contato_adocao[0]});
         } catch (error) {
             return response.status(500).json({confirma : 'Erro', message: error});
         }
--- controllers/publicacaoController.js
+++ controllers/publicacaoController.js
@@ -6,9 +6,13 @@ const db = require("../database/conection");
 module.exports = {
     async listarPublicacao(request, response) {
         try{
-            return response.status(200).json({confirma: 'Publicacao'});
+            const sql = 'SELECT ctt_id, adc_id, ctt_data, ctt_msg from contato_adocao;';
+            const contatoadocao = await db.query(sql);
+
+            return response.status(200).json({confirma: 'Sucesso', nResults: contato_adocao[0]. lenght, message: contato_adocao[0]});
         } catch (error) {
             return response.status(500).json({confirma : 'Erro', message: error});
         }
     },
 };
+
