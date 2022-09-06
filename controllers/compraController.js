754d3f121976ec276c08b201193a171441c8b382
IsabeleAmaris <belinhaamaris@gmail.com>
2022-09-06 11:36:43 -0300

    listarCompra

--- controllers/compraController.js
+++ controllers/compraController.js
@@ -5,7 +5,9 @@ const db = require("../database/conection");
 module.exports = {
     async listarCompra(request, response) {
         try{
-            return response.status(200).json({confirma: 'Compra'});
+            const sql = 'SELECT compra_id, pet_id,cliente_id,endereco,forma_pgto,compra_status from compra;';
+            const compra = await db.query(sql)
+            return response.status(200).json({confirma: 'Sucesso', nResults: compra[0].lenght, message: compra[0]});
         } catch (error) {
             return response.status(500).json({confirma : 'Erro', message: error});
         }
