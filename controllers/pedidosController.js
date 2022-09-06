fbc169c8e401c4e5f167cbe4a415f0f4374a59e1
fferrar1 <110040514+fferrar1@users.noreply.github.com>
2022-09-06 11:36:41 -0300

    listar meu controller
    listar controler

--- controllers/pedidosController.js
+++ controllers/pedidosController.js
@@ -6,7 +6,9 @@ const db = require("../database/conection");
 module.exports = {
     async listarPedidos(request, response) {
         try{
-            return response.status(200).json({confirma: 'Pedidos'});
+            const sql = 'SELECT pedido_id, compra_id, prod_id, pedido_quant, pedido_valor_produto FROM pedidos;';
+            const pedidos = await db.query(sql);
+            return response.status(200).json({confirma: 'Sucesso', nResults: pedidos[0].lenght, message: pedidos[0]});
         } catch (error) {
             return response.status(500).json({confirma : 'Erro', message: error});
         }
