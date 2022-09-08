f3d47b4adc53b2ebafc1f28c8464c5ea8813955a
huguin014 <112496372+huguin014@users.noreply.github.com>
2022-09-08 09:23:44 -0300

    select controllers
    Cliente, Produtos

--- controllers/clienteController.js
+++ controllers/clienteController.js
@@ -6,7 +6,9 @@ const db = require("../database/conection");
 module.exports = {
     async listarCliente(request, response) {
         try{
-            return response.status(200).json({confirma: 'Clientes'});
+            const sql = 'SELECT cliente_id, cliente_email, cliente_senha, cliente_nome, cliente_endereco, cliente_telefone FROM  cliente;';
+            const cliente = await db.query(sql);
+            return response.status(200).json({confirma: 'Sucesso', nResults: cliente[0].lenght, message: cliente[0]});
         } catch (error) {
             return response.status(500).json({confirma : 'Erro', message: error});
         }
--- controllers/produtosController.js
+++ controllers/produtosController.js
@@ -6,7 +6,9 @@ const db = require("../database/conection");
 module.exports = {
     async listarProdutos(request, response) {
         try{
-            return response.status(200).json({confirma: 'Produtos'});
+            const sql = 'SELECT prod_id, pet_id, prod_cod_barras, prod_nome, prod_descricao, prod_marca, prod_valor, prod_estoque, prod_img FROM produtos;';
+            const produtos = await db.query(sql);
+            return response.status(200).json({confirma: 'Sucesso', nResults: produtos[0].lenght, message: produtos[0]});
         } catch (error) {
             return response.status(500).json({confirma : 'Erro', message: error});
         }
