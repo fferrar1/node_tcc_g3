ecf72f16088b2cd8d384b297f311b144b26a1201
Gabrielegava <gabrielegava123@gmail.com>
2022-09-08 09:06:49 -0300

    Executar petshopcontrollers

--- controllers/petshopController.js
+++ controllers/petshopController.js
@@ -5,7 +5,9 @@ const db = require("../database/conection");
 module.exports = {
     async listarPetshop(request, response) {
         try{
-            return response.status(200).json({confirma: 'PetShop'});
+            const sql = 'select pet_id, pet_nome, pet_email, pet_tel_contato, pet_endereco, pet_cidade, pet_estado, pet_senha, pet_logo from petshop;';
+            const petshop = await db.query(sql);
+             return response.status(200).json({confirma: 'Sucesso', nResults: petshop[0].length, message: petshop[0]});
         } catch (error) {
             return response.status(500).json({confirma : 'Erro', message: error});
         }
