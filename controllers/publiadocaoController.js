5d27cacdff1ff7a367a3e5835dd7801c48e0828d
IsabeleAmaris <belinhaamaris@gmail.com>
2022-09-08 09:21:09 -0300

    select 2° controller-publiadocao

--- controllers/publiadocaoController.js
+++ controllers/publiadocaoController.js
@@ -5,7 +5,9 @@ const db = require("../database/conection");
 module.exports = {
     async listarPubliadocao(request, response) {
         try{
-            return response.status(200).json({confirma: 'Publiadocao'});
+            const sql = 'select adc_id,adc_porte,adc_raca,adc_idade,adc_sexo,adc_desc,adc_foto,cliente_id,adc_status,adc_data from publi_adocao;';
+            const publiadocao = await db.query(sql)
+            return response.status(200).json({confirma: 'Sucesso', nResults: publiadocao[0].lenght, message: publiadocao[0]});
         } catch (error) {
             return response.status(500).json({confirma : 'Erro', message: error});
         }
