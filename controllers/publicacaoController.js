7d07c3af50986c81a57262c24a0eede2781f5218
Guzinxd <110039773+Guzinxd@users.noreply.github.com>
2022-09-08 09:23:40 -0300

    publicacao

--- controllers/publicacaoController.js
+++ controllers/publicacaoController.js
@@ -6,10 +6,10 @@ const db = require("../database/conection");
 module.exports = {
     async listarPublicacao(request, response) {
         try{
-            const sql = 'SELECT ctt_id, adc_id, ctt_data, ctt_msg from contato_adocao;';
-            const contatoadocao = await db.query(sql);
-
-            return response.status(200).json({confirma: 'Sucesso', nResults: contato_adocao[0]. lenght, message: contato_adocao[0]});
+            const sql = 'select publi_id, publi_name, pet_id, publi_desc, publi_conteudo, publi_status from publicacao;';
+            const publicacao = await db.query(sql);
+    
+            return response.status(200).json({confirma: 'Sucesso', nResults: publicacao[0].length, message: publicacao[0]});
         } catch (error) {
             return response.status(500).json({confirma : 'Erro', message: error});
         }
