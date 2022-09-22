7d741c341cd2dfd355a7336caed4ac48ff71f90a
huguin014 <112496372+huguin014@users.noreply.github.com>
2022-09-22 09:47:29 -0300

    update cliente e produtos
    trocando os valores das tabelas

--- controllers/clienteController.js
+++ controllers/clienteController.js
@@ -4,16 +4,15 @@ const { json } = require("express");
 const db = require("../database/conection");
 
 module.exports = {
-    async listarCliente(request, response) {
-        try{
-            const sql = 'SELECT cliente_id, cliente_email, cliente_senha, cliente_nome, cliente_endereco, cliente_telefone FROM  cliente;';
-            const cliente = await db.query(sql);
-            return response.status(200).json({confirma: 'Sucesso', nResults: cliente[0].lenght, message: cliente[0]});
-        } catch (error) {
-            return response.status(500).json({confirma : 'Erro', message: error});
-        }
-    },
-
+async listarCliente(request, response) {
+    try{
+    const sql = 'SELECT cliente_id, cliente_email, cliente_senha, cliente_nome, cliente_endereco, cliente_telefone FROM  cliente;';
+    const cliente = await db.query(sql);
+        return response.status(200).json({confirma: 'Sucesso', nResults: cliente[0].lenght, message: cliente[0]});
+    } catch (error) {
+        return response.status(500).json({confirma : 'Erro', message: error});
+    }
+},
 
 async create(request, response) {
     try {
@@ -23,6 +22,19 @@ async create(request, response) {
     const confirmacao = await db.query(sql, values);
     const cliente_id = confirmacao[0].insertId; 
         return response.status(200).json({confirma: 'Sucesso', message: cliente_id});
+    } catch (error) { 
+        return response.status(500).json({confirma: 'Erro', message: error});
+    } 
+},
+
+async update(request, response) {
+    try {
+    const { cliente_email, cliente_senha, cliente_nome, cliente_endereco, cliente_telefone } = request.body; 
+    const { cliente_id } = request.parms;
+    const sql = 'UPDATE cliente SET cliente_email = ?, cliente_senha = ?, cliente_nome = ?, cliente_endereco = ?, cliente_telefone = ? WHERE cliente_id = ?'; 
+    const values = [cliente_email, cliente_senha, cliente_nome, cliente_endereco, cliente_telefone, cliente_id];     
+    const atualizacao = await db.query(sql, values); 
+        return response.status(200).json({confirma: 'Sucesso', message: 'Dados Atualizados'});
     } catch (error) { 
         return response.status(500).json({confirma: 'Erro', message: error});
     } 
--- controllers/produtosController.js
+++ controllers/produtosController.js
@@ -4,15 +4,16 @@ const { json } = require("express");
 const db = require("../database/conection");
 
 module.exports = {
-    async listarProdutos(request, response) {
-        try{
-            const sql = 'SELECT prod_id, pet_id, prod_cod_barras, prod_nome, prod_descricao, prod_marca, prod_valor, prod_estoque, prod_img FROM produtos;';
-            const produtos = await db.query(sql);
-            return response.status(200).json({confirma: 'Sucesso', nResults: produtos[0].lenght, message: produtos[0]});
-        } catch (error) {
-            return response.status(500).json({confirma : 'Erro', message: error});
-        }
-    },
+async listarProdutos(request, response) {
+    try{
+    const sql = 'SELECT prod_id, pet_id, prod_cod_barras, prod_nome, prod_descricao, prod_marca, prod_valor, prod_estoque, prod_img FROM produtos;';
+    const produtos = await db.query(sql);
+        return response.status(200).json({confirma: 'Sucesso', nResults: produtos[0].lenght, message: produtos[0]});
+    } catch (error) {
+        return response.status(500).json({confirma : 'Erro', message: error});
+    }
+},
+
 async create(request, response) {
     try {
     const { pet_id, prod_nome, prod_cod_barras, prod_descricao, prod_marca, prod_valor, prod_estoque, prod_img } = request.body;  
@@ -21,6 +22,19 @@ async create(request, response) {
     const confirmacao = await db.query(sql, values);
     const prod_id = confirmacao[0].insertId; 
         return response.status(200).json({confirma: 'Sucesso', message: prod_id});
+    } catch (error) { 
+        return response.status(500).json({confirma: 'Erro', message: error});
+    } 
+},
+
+async update(request, response) {
+    try {
+    const { pet_id, prod_nome, prod_cod_barras, prod_descricao, prod_marca, prod_valor, prod_estoque, prod_img } = request.body; 
+    const { prod_id } = request.parms;
+    const sql = 'UPDATE produtos SET pet_id = ?, prod_nome = ?, prod_cod_barras = ?, prod_descricao = ?, prod_marca = ?, prod_valor = ?, prod_estoque = ?, prod_img = ? WHERE prod_id = ?;'; 
+    const values = [pet_id, prod_nome, prod_cod_barras, prod_descricao, prod_marca, prod_valor, prod_estoque, prod_img, prod_id];     
+    const atualizacao = await db.query(sql, values); 
+        return response.status(200).json({confirma: 'Sucesso', message: 'Dados Atualizados'});
     } catch (error) { 
         return response.status(500).json({confirma: 'Erro', message: error});
     } 
