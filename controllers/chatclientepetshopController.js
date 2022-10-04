3aba7943070e2c357cfbbd36f3d473145b537c78
Gabrielegava <gabrielegava123@gmail.com>
2022-10-04 10:52:32 -0300

    Update chatclientepetshopController.js

--- controllers/chatclientepetshopController.js
+++ controllers/chatclientepetshopController.js
@@ -42,18 +42,16 @@ async update(request, response) {
         return response.status(500).json({confirma: 'Erro', message: error});
       } 
    }, 
+   
    async delete(request, response) { 
     try {
-            // parâmetro passado via url na chamada da api pelo front-end
+            
         const { chat_id } = request.params;
-
-            // comando de exclusão
         const sql = 'DELETE FROM chat_cliente_petshop WHERE chat_id = ?'; 
-            // definição de array com os parâmetros que receberam os valores do front-end
         const values = [chat_id];
-            // executa a instrução de exclusão no banco de dados    
+        
         await db.query(sql, values);  
-            // Mensagem de retorno no formato JSON
+           
         return response.status(200).json({confirma: 'Sucesso', message:'Mesa com id ' + chat_id + ' excluída com sucesso'}); 
     } catch (error) {
         return response.status(500).json({confirma: 'Erro', message: error});
