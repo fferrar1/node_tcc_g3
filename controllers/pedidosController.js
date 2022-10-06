b78404b9ff0d65aabc2b941e9f80f4e5ced8d1d2
fferrar1 <110040514+fferrar1@users.noreply.github.com>
2022-10-06 09:41:07 -0300

    new select

--- controllers/pedidosController.js
+++ controllers/pedidosController.js
@@ -6,7 +6,7 @@ const db = require("../database/conection");
 module.exports = {
     async listarPedidos(request, response) {
         try{
-            const sql = 'SELECT pedido_id, compra_id, prod_id, pedido_quant, pedido_valor_produto FROM pedidos;';
+            const sql = 'SELECT pdd.pedido_quant, pdd.pedido_valor_produto, cmp.compra_id, cmp.compra_status, pdt.prod_cod_barras, pdt.prod_descricao, pdt.prod_nome, pdt.prod_marca, pdt.prod_valor, pdt.prod_img, pdt.prod_estoque FROM pedidos pdd INNER JOIN compra cmp ON pdd.compra_id = cmp.compra_id INNER JOIN produtos pdt ON pdd.prod_id = pdt.prod_id';
             const pedidos = await db.query(sql);
             return response.status(200).json({confirma: 'Sucesso', nResults: pedidos[0].lenght, message: pedidos[0]});
         } catch (error) {
